7523e17f70ff4bd75eca54feeedb409e80f817b0
Fabien Drault <fdrault@betomorrow.com>
2023-02-27 15:28:22 +0100

    break(import): Use global import for react-native

--- src/render/styled-tagged-text.native.ts
+++ src/render/styled-tagged-text.native.ts
@@ -1,9 +1,7 @@
 import { FunctionComponent } from "react";
-import { StyleProp, TextProps, TextStyle } from "react-native";
+import type { StyleProp, TextProps, TextStyle } from "react-native";
+import ReactNative from "react-native";
 import { renderReactElements } from "./react";
-
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const reactNative = require("react-native");
 
 interface StyledTaggedTextProps extends TextProps {
 	tagsStyle: Record<string, StyleProp<TextStyle>>;
@@ -13,5 +11,5 @@ interface StyledTaggedTextProps extends TextProps {
 export const StyledTaggedText: FunctionComponent<StyledTaggedTextProps> = (props: StyledTaggedTextProps) => {
 	const { children: inputText, tagsStyle = {}, ...other } = props;
 
-	return renderReactElements(reactNative.Text, other, inputText, tagsStyle);
+	return renderReactElements(ReactNative.Text, other, inputText, tagsStyle);
 };
